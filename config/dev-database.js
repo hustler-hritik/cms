577abe63d155ab4386c8bb12348fc1c65bade335
hustler-hritik <104053284+hustler-hritik@users.noreply.github.com>
2022-11-20 23:13:33 +0530

    Update dev-database.js

--- config/dev-database.js
+++ config/dev-database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    mongoDbUrl: 'mongodb://localhost/cms'
+    mongoDbUrl:'mongodb+srv://hritik_cms:hritik@cluster0.9yuaw6i.mongodb.net/data1?retryWrites=true&w=majority'
 
 };
